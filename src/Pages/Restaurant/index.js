4beb21558efd76294358a816b97545f2e1ef552f
Eduardo Leite Ferreira <eduardo.l.fe@poli.ufrj.br>
2022-01-27 23:08:00 -0300

    Restaurant Done

--- src/Pages/Restaurant/index.js
+++ src/Pages/Restaurant/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '../../Components/Header';
 import {Content} from './styles';
+import {Image} from './styles';
+import {Anuncio} from './styles';
 
 export default function Restaurant (){
     return(<div>
         <Header/>
         <Content/>
+        <Image src='https://fotos.vivadecora.com.br/decoracao-restaurante-banquetas-metalizadas-e-parede-de-tijolinho-comum-crispaola-218139-proportional-height_cover_medium.jpg'/>
+        <Anuncio>Visite nosso novo restaurante!</Anuncio>
     </div>);
+        
 }
--- src/Pages/Restaurant/styles.js
+++ src/Pages/Restaurant/styles.js
@@ -4,4 +4,16 @@ export const Content = styled.div`
     width: 100%;
     height: 100%;
     flex-direction: column;
+    margin-top: 5vh;
+`;
+
+export const Image = styled.img`
+    width: 40vw;
+    height: 60vh;
+    margin-left: 380px;
+`;
+
+export const Anuncio = styled.h2`
+    margin-top: 3vh ;
+    margin-left: 380px;
 `;
